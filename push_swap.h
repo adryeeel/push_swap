0f9e86ce6ea359b9469f596ae15561fb1bc85c43
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-04-26 17:24:35 +0100

    Added function signature for stack conversion

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 01:07:13 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/26 02:27:38 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/04/26 17:04:03 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 #include <stdbool.h>
 #include "libft/libft.h"
+
+int *ft_convert_stack(const char *raw_stack[], size_t size);
 
 bool ft_check_nan(const char *stack[], size_t size);
 bool ft_check_args(const char *stack[], size_t size);
