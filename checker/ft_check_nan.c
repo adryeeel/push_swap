3b6326912fadf1893490caa226e2f649443d24ec
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-04-26 15:40:56 +0100

    Fixed check logic

--- checker/ft_check_nan.c
+++ checker/ft_check_nan.c
@@ -6,11 +6,22 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 01:13:28 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/26 02:27:58 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/04/26 15:16:32 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+static bool ft_isnum(const char *s)
+{
+	while (*s)
+	{
+		if (!ft_isdigit(*s) && *s != '-' && *s != '+')
+			return (false);
+		s++;
+	}
+	return (true);
+}
 
 bool ft_check_nan(const char *stack[], size_t size)
 {
@@ -19,7 +30,7 @@ bool ft_check_nan(const char *stack[], size_t size)
 	i = 0;
 	while (i < size)
 	{
-		if (!ft_isdigit(stack[i][0]))
+		if (!ft_isnum(stack[i]))
 			return (false);
 		i++;
 	}
--- checker/ft_check_overflow.c
+++ checker/ft_check_overflow.c
@@ -6,24 +6,23 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 02:09:54 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/26 02:40:00 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/04/26 15:37:32 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits.h>
 #include "../push_swap.h"
-
-#define MAX_INT "2147483647"
-#define MIN_INT "-2147483648"
 
 bool ft_check_overflow(const char *stack[], size_t size)
 {
 	size_t i;
+	long value;
 
 	i = 0;
 	while (i < size)
 	{
-		if (ft_strncmp(MAX_INT, stack[i], ft_strlen(MAX_INT)) < 0 ||
-			ft_strncmp(MIN_INT, stack[i], ft_strlen(MIN_INT)) > 0)
+		value = atol(stack[i]);
+		if (value > INT_MAX || value < INT_MIN)
 			return (false);
 		i++;
 	}
