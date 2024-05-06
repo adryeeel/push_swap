77aa96259c9ff44dbbebde9521995df9e4eb7aa7
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-06 23:54:01 +0100

    Return changed to boolean.

--- stack/ft_swap_stack.c
+++ stack/ft_swap_stack.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_swap_stack.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 23:35:23 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/06 23:36:24 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/06 23:53:11 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void ft_swap(int *stack, size_t size)
+bool ft_swap_stack(int *stack, size_t size)
 {
 	int tmp;
 
 	if (size <= 1)
-		return;
-
+		return (false);
 	tmp = stack[0];
 	stack[0] = stack[1];
 	stack[1] = tmp;
+	return (true);
 }
