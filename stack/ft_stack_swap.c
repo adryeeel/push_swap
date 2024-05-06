bfd33b03897d30ad91ccbb10c9232dd98d4b7c61
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-07 00:43:23 +0100

    Stack swap

--- stack/ft_stack_swap.c
+++ stack/ft_stack_swap.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap_stack.c                                    :+:      :+:    :+:   */
+/*   ft_stack_swap.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 23:35:23 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/06 23:53:11 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/07 00:42:44 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-bool ft_swap_stack(int *stack, size_t size)
+bool ft_stack_convert(int *stack, size_t size)
 {
 	int tmp;
 
