bc4b6efb7e120712536e031c26d16ede5e7d2ee7
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-06 23:57:18 +0100

    Sort operations signatures

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 01:07:13 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/04/26 17:04:03 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/06 23:56:02 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,10 @@ bool ft_check_nan(const char *stack[], size_t size);
 bool ft_check_args(const char *stack[], size_t size);
 bool ft_check_dups(const char *stack[], size_t size);
 bool ft_check_overflow(const char *stack[], size_t size);
+
+void ft_sa(int *stack_a, size_t size_a);
+void ft_sb(int *stack_b, size_t size_b);
+
+bool ft_swap_stack(int *stack, size_t size);
 
 #endif
