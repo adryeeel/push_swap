48a7eb52a7d1baebb82c20de5672256b460dc331
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-07 20:18:52 +0100

    Fixed function name typo

--- stack/ft_stack_swap.c
+++ stack/ft_stack_swap.c
@@ -6,13 +6,13 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 23:35:23 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/07 00:42:44 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/07 20:17:34 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-bool ft_stack_convert(int *stack, size_t size)
+bool ft_stack_swap(int *stack, size_t size)
 {
 	int tmp;
 
