a90807b5918db9892974fc8609dfbb6d8b748306
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-08 00:32:10 +0100

    New logic. memmove instead of loop copy.

--- stack/ft_stack_push.c
+++ stack/ft_stack_push.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 23:38:34 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/07 00:42:52 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/08 00:21:00 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,18 @@ void ft_stack_push(int **stack, int new_value, size_t *size)
 {
 	int *new_stack;
 
-	if (*size == 0)
-		return;
-
 	new_stack = ft_calloc(++(*size), sizeof(int));
 
 	if (!new_stack)
 		return;
 
-	while ((*size)-- > 0)
-		new_stack[*size] = (*stack)[*size];
 	new_stack[0] = new_value;
 
-	free(*stack);
+	if (*stack)
+	{
+		ft_memmove(new_stack + 1, *stack, ((*size) - 1) * sizeof(int));
+		free(*stack);
+	}
+
 	*stack = new_stack;
 }
