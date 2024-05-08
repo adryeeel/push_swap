3c376d016ee11bd39f2521f2bc49d21727cc5e84
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-09 00:22:02 +0100

    push_swap initial

--- push_swap.c
+++ push_swap.c
@@ -6,40 +6,20 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 00:58:17 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/08 01:38:48 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/09 00:21:37 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "push_swap.h"
 
-void ft_print(int *stack, size_t size)
-{
-	while (size-- > 0)
-	{
-		ft_printf("%d ", *stack);
-		stack++;
-	}
-	ft_printf("\n");
-}
-
 int main(int argc, char const *argv[])
 {
-	int *stack_a;
-	size_t sa_size;
-
 	if (argc < 2)
 		return (EXIT_FAILURE);
 
 	if (!ft_check_args(++argv, --argc))
 		return (EXIT_FAILURE);
-
-	sa_size = (size_t)argc;
-	stack_a = ft_stack_convert(argv, argc);
-
-	ft_print(stack_a, sa_size);
-	ft_stack_rrotate(stack_a, sa_size);
-	ft_print(stack_a, sa_size);
 
 	return (EXIT_SUCCESS);
 }
