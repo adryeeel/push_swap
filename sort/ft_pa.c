df1f37b2b573c5db09c9eed87bb634a406df2ead
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-08 01:58:17 +0100

    Error handling

--- sort/ft_pa.c
+++ sort/ft_pa.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 00:14:47 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/08 00:47:30 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/08 01:53:05 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void ft_pa(int **stack_a, int **stack_b, size_t *sa_size, size_t *sb_size)
 {
-	if (*sb_size == 0)
+	if (!*sb_size || !*stack_b)
 		return;
 
 	ft_stack_push(stack_a, **stack_b, sa_size);
--- sort/ft_pb.c
+++ sort/ft_pb.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 00:57:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/08 00:58:29 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/08 01:54:07 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void ft_pb(int **stack_a, int **stack_b, size_t *sa_size, size_t *sb_size)
 {
-	if (*sa_size == 0)
+	if (!*sa_size || !*stack_a)
 		return;
 
 	ft_stack_push(stack_b, **stack_a, sb_size);
