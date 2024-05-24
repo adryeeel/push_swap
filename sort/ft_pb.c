cf21639200c720049e4c875805abb1d206fc4f05
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-25 00:16:17 +0100

    Change to stack struct

--- sort/ft_pb.c
+++ sort/ft_pb.c
@@ -6,19 +6,19 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 00:57:22 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/08 01:54:07 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/25 00:09:30 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void ft_pb(int **stack_a, int **stack_b, size_t *sa_size, size_t *sb_size)
+void ft_pb(t_stack *stack_a, t_stack *stack_b)
 {
-	if (!*sa_size || !*stack_a)
+	if (!stack_a || !stack_a->size)
 		return;
 
-	ft_stack_push(stack_b, **stack_a, sb_size);
-	ft_stack_pop(stack_a, sa_size);
+	ft_stack_push(stack_b, stack_a->data[0]);
+	ft_stack_pop(stack_a);
 
 	ft_putendl_fd("pb", STDOUT_FILENO);
 }
