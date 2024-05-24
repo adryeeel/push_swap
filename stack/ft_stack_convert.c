c397577515923d00cf404765b9ccc214949fe5e2
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-24 22:16:57 +0100

    Refactor to stack struct

--- stack/ft_stack_convert.c
+++ stack/ft_stack_convert.c
@@ -6,20 +6,23 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 16:38:13 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/07 20:17:57 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/24 22:08:30 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-int	*ft_stack_convert(const char **raw_stack, size_t size)
+int *ft_stack_convert(const char **raw_stack, size_t size)
 {
-	int	*int_stack;
+	int *data;
 
-	int_stack = ft_calloc(size, sizeof(int));
-	if (!int_stack)
+	data = ft_calloc(size, sizeof(int));
+
+	if (!data)
 		return (NULL);
+
 	while (size-- > 0)
-		int_stack[size] = ft_atoi(raw_stack[size]);
-	return (int_stack);
+		data[size] = ft_atoi(raw_stack[size]);
+
+	return (data);
 }
