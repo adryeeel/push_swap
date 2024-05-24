17cbe9767bb8555c53a3a3ad1893a45e8c257d3c
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-25 00:30:11 +0100

    Change to stack struct

--- sort/ft_pa.c
+++ sort/ft_pa.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 00:14:47 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/25 00:08:46 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/25 00:25:40 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ void ft_pa(t_stack *stack_a, t_stack *stack_b)
 
 	ft_stack_push(stack_a, stack_b->data[0]);
 	ft_stack_pop(stack_b);
-	
+
 	ft_putendl_fd("pa", STDOUT_FILENO);
 }
