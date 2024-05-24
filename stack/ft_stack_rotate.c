7ae107168b46a843a1b60049be359436bf0226fc
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-24 23:54:38 +0100

    Refactor to stack struct

--- stack/ft_stack_rotate.c
+++ stack/ft_stack_rotate.c
@@ -6,24 +6,22 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 23:38:38 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/08 01:49:51 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/24 18:42:51 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-bool ft_stack_rotate(int *stack, size_t size)
+bool ft_stack_rotate(t_stack *stack)
 {
 	int tmp;
 	
-	if (!stack || !size)
+	if (!stack || !stack->size)
 		return (false);
 
-	tmp = stack[size - 1];
-
-	if (!ft_memmove(stack + 1, stack, (size) * sizeof(int)))
-		return (false);
-
-	stack[0] = tmp;
+	tmp = stack->data[stack->size - 1];
+	ft_memmove(stack + 1, stack, (stack->size) * sizeof(int));
+	stack->data[0] = tmp;
+	
 	return (true);
 }
