21eeb263fff02a553a30eac21e46f92890e60185
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-24 23:56:31 +0100

    Added stack struct

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 01:07:13 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/09 00:24:21 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/24 22:13:02 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,26 @@
 #include <stdbool.h>
 #include "libft/libft.h"
 
+typedef struct s_stack
+{
+	int *data;
+	size_t size;
+
+} t_stack;
+
 bool ft_check_nan(const char *stack[], size_t size);
 bool ft_check_args(const char *stack[], size_t size);
 bool ft_check_dups(const char *stack[], size_t size);
 bool ft_check_overflow(const char *stack[], size_t size);
 
-bool ft_stack_swap(int *stack, size_t size);
-bool ft_stack_pop(int **stack, size_t *size);
-bool ft_stack_rotate(int *stack, size_t size);
-bool ft_stack_rrotate(int *stack, size_t size);
+bool ft_stack_pop(t_stack *stack);
+bool ft_stack_swap(t_stack *stack);
+bool ft_stack_rotate(t_stack *stack);
+bool ft_stack_rrotate(t_stack *stack);
+void ft_stack_destroy(t_stack *stack);
+bool ft_stack_push( t_stack *stack, int new_value);
 int *ft_stack_convert(const char **raw_stack, size_t size);
-bool ft_stack_push(int **stack, int new_value, size_t *size);
+t_stack *ft_stack_create(const char **raw_stack, size_t size);
 
 void ft_sa(int *stack_a, size_t size_a);
 void ft_sb(int *stack_b, size_t size_b);
