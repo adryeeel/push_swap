04c32feeb1322e6e893975ee3c3a692510fb3a00
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-24 23:54:18 +0100

    Refactor to stack struct

--- stack/ft_stack_pop.c
+++ stack/ft_stack_pop.c
@@ -6,26 +6,27 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 00:17:28 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/08 00:50:16 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/24 18:34:57 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-bool ft_stack_pop(int **stack, size_t *size)
+bool ft_stack_pop(t_stack *stack)
 {
-    int *new_stack;
+    int *new_data;
 
-    if (!*size || !*stack)
+    if (!stack->size || !stack)
         return (false);
 
-    new_stack = ft_calloc(--(*size), sizeof(int));
+    new_data = ft_calloc(--(stack->size), sizeof(int));
 
-    if (!new_stack)
+    if (!new_data)
         return (false);
 
-    ft_memmove(new_stack, *stack + 1, (*size) * sizeof(int));
-    free(*stack);
-    *stack = new_stack;
+    ft_memmove(new_data, stack->data + 1, (stack->size) * sizeof(int));
+    free(stack->data);
+    stack->data = new_data;
+
     return (true);
 }
