0ae262f4e3903a368312c8a563a0fb040eb4434b
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-26 00:01:30 +0100

    Added check for sorted stack

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 00:58:17 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/09 00:21:37 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/25 01:07:17 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,9 @@ int main(int argc, char const *argv[])
 
 	if (!ft_check_args(++argv, --argc))
 		return (EXIT_FAILURE);
+
+	if (ft_check_sort(argv, argc))
+		return (EXIT_SUCCESS);
 
 	return (EXIT_SUCCESS);
 }
