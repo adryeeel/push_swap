61943bf75d63ef3876c77beef3046ba31edae3f6
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-28 01:39:23 +0100

    Sort stack at size 3

--- stack/ft_stack_sort.c
+++ stack/ft_stack_sort.c
@@ -6,14 +6,48 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/27 00:02:35 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/27 00:07:42 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/28 01:30:44 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+void ft_sort_three(t_stack *stack_a)
+{
+	if (stack_a->data[0] < stack_a->data[1] &&
+		stack_a->data[1] > stack_a->data[2])
+		ft_rra(stack_a);
+
+	if (stack_a->data[0] > stack_a->data[1] &&
+		stack_a->data[0] > stack_a->data[2])
+		ft_ra(stack_a);
+
+	if (stack_a->data[0] > stack_a->data[1] &&
+		stack_a->data[1] < stack_a->data[2])
+		ft_sa(stack_a);
+}
+
 void ft_stack_sort(t_stack *stack_a, t_stack *stack_b)
 {
-	(void)stack_a;
-	(void)stack_b;
+	size_t size;
+
+	size = stack_a->size;
+
+	if (size == 2)
+	{
+		ft_sa(stack_a);
+		return;
+	}
+
+	if (size <= 3)
+	{
+		ft_sort_three(stack_a);
+		return;
+	}
+
+	while (size-- > 3)
+	{
+		if (stack_b->size < 2)
+			ft_pb(stack_a, stack_b);
+	}
 }
