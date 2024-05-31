90a896c8dbb465d45337deb42931147fc0bef1af
Adryel Bezerra <adryelrbrz+github@gmail.com>
2024-05-31 17:15:56 +0100

    Added initialization for min and max members.

--- stack/ft_stack_create.c
+++ stack/ft_stack_create.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 21:55:11 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/05/27 00:31:44 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/05/31 00:51:14 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,17 @@ t_stack *ft_stack_create(const char **raw_stack, size_t size)
 	if (!stack)
 		return (NULL);
 
+	stack->max = 0;
+	stack->min = 0;
 	stack->size = size;
+	stack->data = NULL;
 
 	if (raw_stack)
+	{
 		stack->data = ft_stack_convert(raw_stack, size);
-	else
-		stack->data = NULL;
+		stack->max = ft_stack_max(stack);
+		stack->min = ft_stack_min(stack);
+	}
 
 	return (stack);
 }
