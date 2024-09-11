ac822817f892055e7cb48f79d22730b6a14bfb2a
adryeeel <adryelrbrz+github@gmail.com>
2024-09-11 01:32:29 +0100

    Add personal libraries to support project development

---
+++ libs/strarr/ft_strarr_create.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strarr_create.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 00:55:58 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/10 00:57:50 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_strarr.h"
+
+t_strarr *ft_strarr_create(void)
+{
+	t_strarr *new;
+
+	new = ft_calloc(1, sizeof(t_strarr));
+	
+	if (!new)
+		return (NULL);
+	
+	new->length = 0;
+	new->data = NULL;
+
+	return (new);
+}
---
+++ libs/strarr/ft_strarr_insertat.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strarr_insertat.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 19:13:46 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/08 23:43:21 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_strarr.h"
+
+char **ft_strarr_insertat(char *strarr[], char *str, size_t index)
+{
+	(void)strarr;
+	(void)str;
+	(void)index;
+	return (NULL);
+}
