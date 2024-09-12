a3a1ad0f5b2adc5843646e2825182d70d32ba1ca
adryeeel <adryelrbrz+github@gmail.com>
2024-09-12 02:35:22 +0100

    Add function to create stack data structure

---
+++ push_swap/stack/ft_stack_create.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stack_create.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 21:55:11 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/09 22:10:39 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+t_stack *ft_stack_create(void)
+{
+	t_stack *stack;
+
+	stack = malloc(sizeof(t_stack));
+
+	if (!stack)
+		return (NULL);
+
+	return (stack);
+}
