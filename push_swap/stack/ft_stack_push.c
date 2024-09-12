1ec47d672f8aa7e6d9e2dd752e82e79c9034a95b
adryeeel <adryelrbrz+github@gmail.com>
2024-09-12 02:39:54 +0100

    Add function to add an element to stack

---
+++ push_swap/stack/ft_stack_push.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stack_push.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/06 23:38:34 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/08/21 16:22:26 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void ft_stack_push(t_stack *stack, int new_value)
+{
+	int *new_stack;
+
+	new_stack = ft_calloc(++(stack->size), sizeof(int));
+
+	if (!new_stack)
+		return;
+
+	new_stack[0] = new_value;
+
+	if (stack->data)
+	{
+		ft_memmove(new_stack + 1, stack->data, ((stack->size) - 1) * sizeof(int));
+		free(stack->data);
+	}
+
+	stack->data = new_stack;
+
+	if (stack->size == 1)
+		return;
+
+	stack->max++;
+	stack->min++;
+
+	if (new_value > stack->data[stack->max])
+		stack->max = 0;
+
+	if (new_value < stack->data[stack->min])
+		stack->min = 0;
+}
