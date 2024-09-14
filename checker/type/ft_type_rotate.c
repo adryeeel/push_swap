91729d17fde4c5ff9ead6dd4438dc64ce05c86d0
adryeeel <adryelrbrz+github@gmail.com>
2024-09-14 14:05:16 +0100

    Validate if the input action is a rotate type (ra, rb or rr)

---
+++ checker/type/ft_type_rotate.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_type_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/09 17:20:23 by arocha-b          #+#    #+#             */
+/*   Updated: 2024/09/10 00:10:21 by arocha-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../checker.h"
+
+t_action ft_type_rotate(char *act)
+{
+	if (!ft_strncmp("ra\n", act, 3))
+		return (RA);
+
+	if (!ft_strncmp("rb\n", act, 3))
+		return (RB);
+
+	if (!ft_strncmp("rr\n", act, 3))
+		return (RR);
+
+	return (-1);
+}
