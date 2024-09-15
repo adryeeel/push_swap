18a81b7c228e24b901f160b949bc7fb0a03aa842
adryeeel <adryelrbrz+github@gmail.com>
2024-09-15 21:28:42 +0100

    Remove unimplemented lib functions

--- libs/intarr/ft_intarr.h
+++ libs/intarr/ft_intarr.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 19:13:15 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/09 21:13:44 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/15 21:20:29 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,5 @@ typedef struct intarr
 t_intarr *ft_intarr_create(void);
 void ft_intarr_free(t_intarr *intarr);
 t_intarr *ft_intarr_push(t_intarr *intarr, int value);
-
-int ft_intarr_pop(int *intarr);
-int *ft_intarr_copy(int *intarr);
-int ft_intarr_shift(int *intarr);
-int *ft_intarr_sort(int *intarr);
-int *ft_intarr_map(int *intarr, int (*f)(int));
-void ft_intarr_unshift(int *intarr, int value);
-int *ft_intarr_join(int *intarr_a, int *intarr_b);
-void ft_intarr_swap(int *intarr, size_t x, size_t y);
-int *ft_intarr_insertat(int *intarr, size_t value, size_t index);
 
 #endif
--- libs/strarr/ft_strarr.h
+++ libs/strarr/ft_strarr.h
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 19:14:12 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/10 01:15:55 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/15 21:21:33 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,5 @@ void ft_strarr_free(t_strarr *strarr);
 size_t ft_strarr_length(char *strarr[]);
 t_strarr *ft_strarr_push(t_strarr *strarr, char *str);
 t_strarr *ft_strarr_merge(t_strarr *strarr_a, t_strarr *strarr_b);
-
-char **ft_strarr_pop(char *strarr[]);
-char **ft_strarr_copy(char *strarr[]);
-char **ft_strarr_sort(char *strarr[]);
-char **ft_strarr_shift(char *strarr[]);
-char *ft_strarr_join(char *strarr[], char sep);
-char **ft_strarr_unshift(char *strarr[], char *str);
-void ft_strarr_swap(char *strarr[], size_t x, size_t y);
-char **ft_strarr_map(char *strarr[], char *(*f)(char *));
-char **ft_strarr_insertat(char *strarr[], char *str, size_t index);
 
 #endif
