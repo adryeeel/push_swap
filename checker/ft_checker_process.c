019f25fbd3da900cc64e8af5e223af4d51d1221d
adryeeel <adryelrbrz+github@gmail.com>
2024-09-24 07:53:47 +0100

    Refactor the return type to boolean

--- checker/ft_checker_process.c
+++ checker/ft_checker_process.c
@@ -6,23 +6,25 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 00:15:21 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/10 02:01:43 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/24 06:07:43 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-void	ft_checker_process(t_strarr *input)
+bool	ft_checker_process(t_strarr *input)
 {
 	t_intarr	*acts;
 	t_stack		*stack_a;
 	t_stack		*stack_b;
 
 	acts = ft_acts_read();
+	if (!acts)
+		return (false);
 	stack_a = ft_stack_create();
 	stack_b = ft_stack_create();
 	if (!stack_a || !stack_b)
-		return ;
+		return (false);
 	ft_stack_init(&stack_b, NULL);
 	ft_stack_init(&stack_a, input->data);
 	ft_acts_exec(acts, stack_a, stack_b);
@@ -33,4 +35,5 @@ void	ft_checker_process(t_strarr *input)
 	ft_intarr_free(acts);
 	ft_stack_destroy(stack_a);
 	ft_stack_destroy(stack_b);
+	return (true);
 }
