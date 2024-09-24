433b5d75e61158256b383a9894e3b941c45d7bcd
adryeeel <adryelrbrz+github@gmail.com>
2024-09-24 07:50:14 +0100

    Fix error handling for input lines not terminated in \n

--- checker/acts/ft_acts_read.c
+++ checker/acts/ft_acts_read.c
@@ -6,7 +6,7 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 14:40:10 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/09 21:41:28 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/24 07:45:47 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,16 @@ t_intarr	*ft_acts_read(void)
 	t_intarr	*acts;
 
 	acts = ft_intarr_create();
-	if (!acts)
-		return (NULL);
 	act = ft_acts_readl();
+	if (!act || !acts)
+		return (NULL);
 	while (act)
 	{
 		if (!ft_acts_check(act))
 		{
 			free(act);
 			ft_intarr_free(acts);
-			ft_putendl_fd("Error", STDERR_FILENO);
-			exit(EXIT_FAILURE);
+			return (NULL);
 		}
 		ft_acts_push(&acts, act);
 		free(act);
