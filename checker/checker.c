b3106a90aa6b4e6f1615b94fe4ebbe9fae57c7ba
adryeeel <adryelrbrz+github@gmail.com>
2024-09-24 07:52:33 +0100

    Fix memory leak and remove checking for sorted stack

--- checker/checker.c
+++ checker/checker.c
@@ -6,15 +6,15 @@
 /*   By: arocha-b <arocha-b@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/07 19:57:08 by arocha-b          #+#    #+#             */
-/*   Updated: 2024/09/10 02:00:25 by arocha-b         ###   ########.fr       */
+/*   Updated: 2024/09/24 06:11:04 by arocha-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-int	main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
-	t_strarr	*input;
+	t_strarr *input;
 
 	if (argc-- < 2)
 		return (EXIT_FAILURE);
@@ -24,12 +24,12 @@ int	main(int argc, char const *argv[])
 		ft_strarr_free(input);
 		return (EXIT_FAILURE);
 	}
-	if (ft_check_sort(input))
+	if (!ft_checker_process(input))
 	{
 		ft_strarr_free(input);
-		return (EXIT_SUCCESS);
+		ft_putendl_fd("Error", STDERR_FILENO);
+		return (EXIT_FAILURE);
 	}
-	ft_checker_process(input);
 	ft_strarr_free(input);
 	return (EXIT_SUCCESS);
 }
